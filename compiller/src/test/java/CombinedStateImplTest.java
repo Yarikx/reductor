aa5f0764a5aac5dba0bc693c929a60a91f14c7ca
Yaroslav Heriatovych <Yarikx@gmail.com>
2016-09-20 11:59:33 +0100

    add test to cover default and static mehtods in @CombinedState

--- compiller/src/test/java/CombinedStateImplTest.java
+++ compiller/src/test/java/CombinedStateImplTest.java
@@ -53,4 +53,80 @@ public class CombinedStateImplTest {
                 .and()
                 .generatesSources(generatedPojo);
     }
+
+    @Test
+    public void testDoNotProcessStaticMethods() {
+        JavaFileObject source = JavaFileObjects.forSourceString("test.Foobar", "package test;\n" +
+                "\n" +
+                "import com.yheriatovych.reductor.annotations.CombinedState;\n" +
+                "\n" +
+                "@CombinedState\n" +
+                "public interface Foobar {\n" +
+                "    int foo();\n" +
+                "    static String bar() {\n" +
+                "        return \"bar\";\n" +
+                "    }\n" +
+                "}");
+
+        JavaFileObject generatedPojo = JavaFileObjects.forSourceString("test.FoobarImpl", "package test;\n" +
+                "\n" +
+                "import java.lang.Override;\n" +
+                "\n" +
+                "public final class FoobarImpl implements Foobar {\n" +
+                "  private final int foo;\n" +
+                "\n" +
+                "  public FoobarImpl(int foo) {\n" +
+                "    this.foo = foo;\n" +
+                "  }\n" +
+                "\n" +
+                "  @Override\n" +
+                "  public int foo() {\n" +
+                "    return foo;\n" +
+                "  }\n" +
+                "}");
+
+        assertAbout(javaSource()).that(source)
+                .processedWith(new CombinedStateProcessor())
+                .compilesWithoutWarnings()
+                .and()
+                .generatesSources(generatedPojo);
+    }
+
+    @Test
+    public void testDoNotProcessDefaultMethods() {
+        JavaFileObject source = JavaFileObjects.forSourceString("test.Foobar", "package test;\n" +
+                "\n" +
+                "import com.yheriatovych.reductor.annotations.CombinedState;\n" +
+                "\n" +
+                "@CombinedState\n" +
+                "public interface Foobar {\n" +
+                "    int foo();\n" +
+                "    default String bar() {\n" +
+                "        return \"bar\" + foo();\n" +
+                "    }\n" +
+                "}");
+
+        JavaFileObject generatedPojo = JavaFileObjects.forSourceString("test.FoobarImpl", "package test;\n" +
+                "\n" +
+                "import java.lang.Override;\n" +
+                "\n" +
+                "public final class FoobarImpl implements Foobar {\n" +
+                "  private final int foo;\n" +
+                "\n" +
+                "  public FoobarImpl(int foo) {\n" +
+                "    this.foo = foo;\n" +
+                "  }\n" +
+                "\n" +
+                "  @Override\n" +
+                "  public int foo() {\n" +
+                "    return foo;\n" +
+                "  }\n" +
+                "}");
+
+        assertAbout(javaSource()).that(source)
+                .processedWith(new CombinedStateProcessor())
+                .compilesWithoutWarnings()
+                .and()
+                .generatesSources(generatedPojo);
+    }
 }
