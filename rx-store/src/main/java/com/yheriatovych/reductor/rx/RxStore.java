a828cf14f3eaca0da599c8eb2d0721651602e077
Yaroslav Heriatovych <Yarikx@gmail.com>
2016-10-10 10:23:31 +0100

    update rxjava to 1.2.1

--- rx-store/src/main/java/com/yheriatovych/reductor/rx/RxStore.java
+++ rx-store/src/main/java/com/yheriatovych/reductor/rx/RxStore.java
@@ -2,7 +2,7 @@ package com.yheriatovych.reductor.rx;
 
 import com.yheriatovych.reductor.Cancelable;
 import com.yheriatovych.reductor.Store;
-import rx.AsyncEmitter;
+import rx.Emitter;
 import rx.Observable;
 
 public final class RxStore {
@@ -13,10 +13,10 @@ public final class RxStore {
      * Note: This method will emit current sate immediately after subscribe
      */
     public static <State> Observable<State> asObservable(final Store<State> store) {
-        return Observable.fromAsync(stateAsyncEmitter -> {
-            stateAsyncEmitter.onNext(store.getState());
-            final Cancelable cancelable = store.subscribe(stateAsyncEmitter::onNext);
-            stateAsyncEmitter.setCancellation(cancelable::cancel);
-        }, AsyncEmitter.BackpressureMode.LATEST);
+        return Observable.fromEmitter(emitter -> {
+            emitter.onNext(store.getState());
+            final Cancelable cancelable = store.subscribe(emitter::onNext);
+            emitter.setCancellation(cancelable::cancel);
+        }, Emitter.BackpressureMode.LATEST);
     }
 }
