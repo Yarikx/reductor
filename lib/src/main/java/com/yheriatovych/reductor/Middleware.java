6ab9a9363c4ecf9c608171ffe728f6128cc8dfae
Yaroslav Heriatovych <Yarikx@gmail.com>
2016-11-24 17:36:44 +0000

    improved docs for middleware

--- lib/src/main/java/com/yheriatovych/reductor/Middleware.java
+++ lib/src/main/java/com/yheriatovych/reductor/Middleware.java
@@ -14,11 +14,28 @@ package com.yheriatovych.reductor;
  * <li> Use store to obtain state via {@link Store#getState()}
  * <li> Dispatch one or more events to Store via {@link Store#dispatch(Object)} to dispatch through full middleware chain
  * </ul>
+ * <p>
+ * Logging middleware example:
+ * <pre><code>
+ * public class LoggingMiddleware&lt;T&gt; implements Middleware&lt;T&gt; {
+ *     &#64;Override
+ *     public Dispatcher create(Store&lt;T&gt; store, Dispatcher nextDispatcher) {
+ *         return action -&gt; {
+ *             log("dispatching action: " + action);
+ *             nextDispatcher.dispatch(action);
+ *             log("new state: " + store.getState());
+ *         };
+ *     }
+ * }
+ * </code></pre>
  */
 public interface Middleware<State> {
     /**
      * Create and initialize Dispatcher.
      * Called only once during {@link Store} creation.
+     * <p>
+     * {@code nextDispatcher} is intended to be used by returned Dispatcher
+     * to proceed with dispatch chain
      *
      * @param store          original store object
      * @param nextDispatcher callback to be called by middleware to proceed with dispatch chain
