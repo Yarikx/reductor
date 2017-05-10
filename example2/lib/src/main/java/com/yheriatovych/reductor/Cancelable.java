e9ae86a17b789f94af7bf6d6ee80470ae52be881
鄭淵舟 <zhengyuanzhou@Josephs-MacBook-Pro.local>
2017-05-10 15:11:17 +0800

    remove unneeded file

--- example2/lib/src/main/java/com/yheriatovych/reductor/Cancelable.java
+++
@@ -1,8 +0,0 @@
-package com.yheriatovych.reductor;
-
-/**
- * Returned from {@link Cursor#subscribe(StateChangeListener)} to allow unsubscribing.
- */
-public interface Cancelable {
-    void cancel();
-}
--- example2/lib/src/main/java/com/yheriatovych/reductor/Function.java
+++
@@ -1,5 +0,0 @@
-package com.yheriatovych.reductor;
-
-public interface Function<T, R> {
-    R apply(T value);
-}
